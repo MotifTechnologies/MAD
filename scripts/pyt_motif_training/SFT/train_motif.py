014da4778703f74abcabcb821755dbe409c1a588
junhyeok.lee <junhyeok.lee@moreh.io>
2025-09-08 19:13:47 +0900

    fix: fix split name

--- scripts/pyt_motif_training/SFT/train_motif.py
+++ scripts/pyt_motif_training/SFT/train_motif.py
@@ -47,7 +47,7 @@ def main(args):
 
     # this demo will use 100 samples of origin data
     # downloading the dataset will consume about 2.5 gb of your storage
-    train_dataset = load_dataset("HuggingFaceH4/ultrachat_200k", split="train[:100]")
+    train_dataset = load_dataset("HuggingFaceH4/ultrachat_200k", split="train_sft[:100]")
     total_iters = len(train_dataset) // (args.batchsize * accelerator.state.num_processes)
 
     # loading model
